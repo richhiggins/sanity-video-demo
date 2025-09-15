22592595dd08a9c52f79a1cf4056b722be66e474
Rich Higgins <hello@iamrichhiggins.com>
2025-09-15 17:02:16 +0100

    bumped studio & vision versions, customDomain & maxResolution props

--- frontend/app/components/Video.tsx
+++ frontend/app/components/Video.tsx
@@ -8,6 +8,8 @@ const Video = async ({ video_ref }: { video_ref: string }) => {
   return (
     <MuxPlayer
       playbackId={playbackInfo.id}
+      customDomain="m.sanity-cdn.com"
+      maxResolution="1080p"
       style={{
         width: "100%",
         height: "100%",
